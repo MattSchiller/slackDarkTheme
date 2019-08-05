fd90b1ee38d337d0fb1eba3130dd7a5e42ef8e55
Matt Schiller <matt.schiller@tumblr.com>
2019-08-04 21:49:22 -0400

    Completely replaced prior styles and realized that I'll have to manually setup the css embed

--- slack-dark-mode.sh
+++ slack-dark-mode.sh
@@ -1,30 +1,28 @@
-#!/usr/bin/env bash
-
 JS="
 // First make sure the wrapper app is loaded
 document.addEventListener('DOMContentLoaded', function() {
   // Fetch our CSS in parallel ahead of time
-  const cssPath =
-    'https://raw.githubusercontent.com/MattSchiller/slackDarkTheme/master/css/slackDarkTheme.css';
-  let cssPromise = fetch(cssPath).then(response => response.text());
+  const cssPath = 'https://cdn.rawgit.com/widget-/slack-black-theme/master/custom.css';
+  
+  let cssPromise = fetch(cssPath).then((response) => response.text());
 
-  let customCustomCSS = \`
+  let customCustomCSS = '
    :root {
       /* Modify these to change your theme colors: */
-      --primary: #61AFEF;
-      --text: #ABB2BF;
-      --background: #282C34;
-      --background-elevated: #3B4048;
+      --primary: #09F;
+      --text: #CCC;
+      --background: #080808;
+      --background-elevated: #222;
    }
-   \`;
+   '
 
   // Insert a style tag into the wrapper view
-   cssPromise.then((css) => {
+  cssPromise.then((css) => {
     let s = document.createElement('style');
     s.type = 'text/css';
     s.innerHTML = css + customCustomCSS;
     document.head.appendChild(s);
-   });
+  });
 });"
 
 SLACK_RESOURCES_DIR="/Applications/Slack.app/Contents/Resources"
@@ -33,6 +31,6 @@ SLACK_FILE_PATH="${SLACK_RESOURCES_DIR}/app.asar.unpacked/dist/ssb-interop.bundl
 echo "Adding Dark Theme Code to Slack... "
 echo "This script requires sudo privileges." && echo "You'll need to provide your password."
 
-sudo npx asar extract ${SLACK_RESOURCES_DIR}/app.asar ${SLACK_RESOURCES_DIR}/app.asar.unpacked
-sudo tee -a "${SLACK_FILE_PATH}" > /dev/null <<< "$JS"
+# sudo npx asar extract ${SLACK_RESOURCES_DIR}/app.asar ${SLACK_RESOURCES_DIR}/app.asar.unpacked
+# sudo tee -a "${SLACK_FILE_PATH}" > /dev/null <<< "$JS"
 sudo npx asar pack ${SLACK_RESOURCES_DIR}/app.asar.unpacked ${SLACK_RESOURCES_DIR}/app.asar
